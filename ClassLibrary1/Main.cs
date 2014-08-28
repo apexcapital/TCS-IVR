0c7dc91d14bac89b3ba61e553a0ae3b1e49b65b8
Josh Mabry <josh.mabry@transconnectservices.com>
2014-08-28 10:01:09 -0500

    initial commit

---
+++ ClassLibrary1/Main.cs
@@ -0,0 +1,26 @@
+﻿using BuildABot.UC;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class Main
+    {
+        static void Main(string[] args)
+        {
+            // Print all Debug.WriteLine calls to the console to make it
+            // easier to determine the cause of any errors.
+            Debug.Listeners.Add(new ConsoleTraceListener());
+
+            String applicationUserAgent = ConfigurationManager.AppSettings["applicationuseragent"];
+            String applicationurn = ConfigurationManager.AppSettings["applicationurn"];
+            UCBotHost ucBotHost = new UCBotHost(applicationUserAgent, applicationurn);
+            ucBotHost.Run();
+        }
+    }
+}
---
+++ ClassLibrary1/TimeMessageHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using BuildABot.Core.MessageHandlers;
+
+namespace ClassLibrary1
+{
+    [Export(typeof(MessageHandler))]
+    class TimeMessageHandler : MessageHandler
+    {
+        public TimeMessageHandler()
+            :base("time","Please wait while I get the current time...", true)
+{
+}
+        public override MessageHandlingResponse CanHandle(Message message)
+        {
+            MessageHandlingResponse response = new MessageHandlingResponse();
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Saturday
+                || DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                response.Confidence = 0;
+            }
+            else
+            {
+                // We could have implemented more complex/specific
+                // logic for handling the message here.
+                response = base.CanHandle(message);
+            }
+
+            return response;
+        }
+
+    }
+}
